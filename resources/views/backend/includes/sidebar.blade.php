752eb6988cc26c7b77ce052ab846a7412e6739ec
janiniraj <janiniraj1992@gmail.com>
2018-01-25 22:32:41 +0800

    quote setting sidebar

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -19,6 +19,7 @@
             <li><a href="{{ route('admin.dashboard') }}"><i class="fa fa-laptop"></i> <span>Dashboard</span></a></li>
             <li><a href="{{ route('admin.page.index') }}"><i class="fa fa-paper-plane-o"></i> <span>Page</span></a></li>
             <li><a href="{{ route('admin.booking.index') }}"><i class="fa fa-calendar"></i> <span>Booking</span></a></li>
+            <li><a href="{{ route('admin.quotesetting.singleitem.get') }}"><i class="fa fa-calendar"></i> <span>Single Item Setting</span></a></li>
 
             <!-- begin sidebar minify button -->
             <li><a href="javascript:;" class="sidebar-minify-btn" data-click="sidebar-minify"><i class="fa fa-angle-double-left"></i></a></li>
